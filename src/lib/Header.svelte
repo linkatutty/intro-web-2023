eeaf93c3ceb1eb556e015f292c28ed2ed041ec31
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-08-22 12:02:24 +1200

    href gallery and timeline in menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,7 +5,8 @@
     <div class="menu">
       <li><a href="/">home</a></li>
       <li><a href="/about">about</a></li>    
-      <li><a href="/contact">contact</a></li>
+      <li><a href="/gallery">gallery</a></li>
+      <li><a href="/timeline">timeline</a></li>
     </div>
 </header>
 
