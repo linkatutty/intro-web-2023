d2769595a416301b95fc1cca9942a9222b50771c
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-08-30 11:04:44 +1200

    divided about page into ramdom grid sizes, needs to be changes at later date, added notes to to-do

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,8 @@
-<div id="about">
-    <h1> this is the about page! </h1>
+<div class="about-container">
+    <main>main</main>
+    <div id="sidebar">sidebar</div>
+    <div id="content1">content1</div>
+    <div id="content2">content2</div>
+    <div id="content3">content3</div>
+    <footer>footer</footer>
 </div>
