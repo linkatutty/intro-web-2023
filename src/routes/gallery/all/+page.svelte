c16239735083da8e299d591736efe75f99fcfc90
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-09-04 18:31:00 +1200

    started working on gallery all page

---
+++ src/routes/gallery/all/+page.svelte
@@ -0,0 +1,18 @@
+<div class="location-tokyo">
+    <div class="location-header">
+        <div class="location-header_date">
+            <div class="location-header_date-day">day</div>
+            <div class="location-header_date-month">month</div>
+            <div class="location-header_date-year">year</div>
+        </div>
+        <div class="location-title">
+            <span class="location-title-name" style="font-size: 161px; letter-spacing: 0px; white-space: nowrap; visibility: hidden;">
+                 東京
+            </span>
+        </div>
+    </div>
+
+    <div class="1-grid">
+        <div></div>
+    </div>
+</div>
