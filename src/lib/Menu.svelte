dfec7dfd2c26fe14820bbef5b434dfa72c02be4a
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-09-08 11:19:35 +1200

    i tried to write my own css from scratch for new buttons and n ow im lost and confused! thumbs up!

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -8,15 +8,6 @@
         <div></div>
       </label>
   </div>
--->
-
-<button type="button" class="menu-button">
-  <div class="menu-button-ellipsis">
-    <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 128 512">
-    <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg> 
-    <i class="fa-solid fa-ellipsis-vertical"></i>
-  </div>
-</button>
 
 <div class="overlay">
    <input type="radio" name="toggle" id="radio2">
@@ -25,6 +16,34 @@
       <div></div>
     </label>
   </div>
+  <nav>
+   <ul class="menu-list">
+     <li href="/">home</li>
+     <li href="/gallery">gallery</li>
+     <li href="/timeline">timeline</li>
+     <li href="/map">map</li>
+     <li href="/about">about</li>
+   </ul>
+  </nav>
+</div>
+-->
+
+<button type="button" class="menu-open-button">
+  <div class="menu-open">
+    <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 128 512">
+    <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg> 
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+  </div>
+</button>
+
+<div class="menu-overlay">
+   <button type="button" class="menu-close-button">
+    <div class="menu-close">
+      <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512">
+      <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+      <i class="fa-solid fa-minus"></i>
+    </div>
+   </button>
   <nav>
    <ul class="menu-list">
      <li href="/">home</li>
