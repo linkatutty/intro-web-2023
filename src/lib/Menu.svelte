eacbfe7c5951b65e43a4371653dd8ae1181b5f0e
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-09-12 11:37:52 +1200

    tried to fix my menu button to no avail

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -28,21 +28,15 @@
 </div>
 -->
 
-<button type="button" class="menu-open-button">
-  <!-- <div class="menu-open"> -->
-    <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 128 512">
-      <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
-    </svg> 
+<button type="button" class="menu-button-open">
+  <div class="menu-open">
     <i class="fa-solid fa-ellipsis-vertical"></i>
-  <!-- </div> -->
+  </div>
 </button>
 
 <div class="menu-overlay">
-   <button type="button" class="menu-close-button">
+   <button type="button" class="menu-button-close">
     <div class="menu-close">
-      <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512">
-        <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
-      </svg>
       <i class="fa-solid fa-minus"></i>
     </div>
    </button>
@@ -50,6 +44,8 @@
    <ul class="menu-list">
      <li href="/">home</li>
      <li href="/gallery">gallery</li>
+     <!-- the dots before the words are still visible, need to remove in css
+    links also dont take to page so much also be fixed -->
      <li href="/timeline">timeline</li>
      <li href="/map">map</li>
      <li href="/about">about</li>
