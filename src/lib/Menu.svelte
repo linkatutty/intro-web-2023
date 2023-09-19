b29a380a67a8de71a10a3b93e5f39a8e4d1d5426
linkatutty <linkaanne@hotmail.co.nz>
2023-09-19 18:26:43 +1200

    fixed menu open button to be in nicer position, now need to make menu button function and have menu close on click

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,39 +1,7 @@
-<!-- potential menu option -->
-
-<!-- <input type="radio" name="toggle" id="radio1">
-  <div class="container">
-      <label for="radio1" class="ham-menu">
-        <div></div>
-        <div></div>
-        <div></div>
-      </label>
-  </div>
-
-<div class="overlay">
-   <input type="radio" name="toggle" id="radio2">
-  <div class="container">
-    <label for="radio2" class="cross"> 
-      <div></div>
-    </label>
-  </div>
-  <nav>
-   <ul class="menu-list">
-     <li href="/">home</li>
-     <li href="/gallery">gallery</li>
-     <li href="/timeline">timeline</li>
-     <li href="/map">map</li>
-     <li href="/about">about</li>
-   </ul>
-  </nav>
-</div>
--->
-
-
-
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
 <div class="menu-wrap">
   <input type="checkbox" class="toggler">
-    <div class="hamburger">
+    <div class="menu open">
       <i class="fa-solid fa-ellipsis-vertical"/>
     </div>
     <div class="menu">
@@ -46,16 +14,11 @@
              <li><a href="/timeline">timeline</a></li>
              <li><a href="/about">about</a></li>
            </ul>
-           <div class="menu-close">
-            <i class="fa-solid fa-minus"></i>
           </div>
         </div>
       </div>
     </div>
-</div>
-
-
-<!-- <div class="menu-button-cont">
-</div> -->
-
- 
+    
+    <div class="menu-close">
+     <i class="fa-solid fa-minus"></i>
+   </div>
