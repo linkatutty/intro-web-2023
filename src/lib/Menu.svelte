9ce1886f3786d62974e219a0f210f7f17515a749
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-09-20 09:52:17 +1200

    worked more on menu, ntill need to work on

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,24 +1,24 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
 <div class="menu-wrap">
-  <input type="checkbox" class="toggler">
-    <div class="menu open">
-      <i class="fa-solid fa-ellipsis-vertical"/>
-    </div>
-    <div class="menu">
+  <input type="checkbox" class="toggler" />
+  <div class="menu-open">
+    <i class="fa-solid fa-ellipsis-vertical"/>
+  </div>
+  <div class="menu">
+    <div>
       <div>
-        <div>
-           <ul>
-             <li><a href="/">home</a></li>
-             <li><a href="/gallery">gallery</a></li>
-             <li><a href="/map">map</a></li>
-             <li><a href="/timeline">timeline</a></li>
-             <li><a href="/about">about</a></li>
-           </ul>
-          </div>
+          <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="/gallery">gallery</a></li>
+            <li><a href="/map">map</a></li>
+            <li><a href="/timeline">timeline</a></li>
+            <li><a href="/about">about</a></li>
+          </ul>
         </div>
       </div>
     </div>
-    
-    <div class="menu-close">
-     <i class="fa-solid fa-minus"></i>
-   </div>
+  </div>
+  
+  <div class="menu-close">
+    <i class="fa-solid fa-minus"></i>
+  </div>
