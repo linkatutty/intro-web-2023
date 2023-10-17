ae7f9d0a9226d8175b86702832e7dd9c16bee9e1
Linka Tutty (1000120514) <TUTTLA1@student.op.ac.nz>
2023-10-17 13:35:41 +1300

    started adding photos to gallery

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,3 +1,4 @@
+
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
 <div class="menu-wrap">
   <input type="checkbox" class="toggler" />
@@ -7,18 +8,18 @@
   <div class="menu">
     <div>
       <div>
-          <ul>
-            <li><a href="/">home</a></li>
-            <li><a href="/gallery">gallery</a></li>
-            <li><a href="/map">map</a></li>
-            <li><a href="/timeline">timeline</a></li>
-            <li><a href="/about">about</a></li>
-          </ul>
-        </div>
+        <ul>
+          <li><a href="/">home</a></li>
+          <li><a href="/gallery">gallery</a></li>
+          <li><a href="/map">map</a></li>
+          <li><a href="/timeline">timeline</a></li>
+          <li><a href="/about">about</a></li>
+        </ul>
       </div>
     </div>
   </div>
+</div>
   
-  <div class="menu-close">
-    <i class="fa-solid fa-minus"></i>
-  </div>
+<div class="menu-close">
+  <i class="fa-solid fa-minus"></i>
+</div>
--- src/routes/gallery/all/+page.svelte
+++ src/routes/gallery/all/+page.svelte
@@ -18,4 +18,18 @@
             <img class="image_tokyo photo" src="/img/japan-photo.png" alt="photo of a trip in japan"> 
         </div>
     </div>
+    <div class="photo-grid">
+        <div class="japan=photo x1-5 y1-3">
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img class="image-tokyo photo" src="/img/mito-shrine1.png" alt="photo of a trip in japan">
+        </div>
+        <div class="japan=photo x6-4 y2-2">
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img class="image-tokyo photo" src="/img/mito-shrine2.png" alt="photo of a trip in japan">
+        </div>
+        <div class="japan=photo x4-4 y4-3">
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img class="image-tokyo photo" src="/img/mito-shrine3.png" alt="photo of a trip in japan">
+        </div>
+    </div>
 </div>
